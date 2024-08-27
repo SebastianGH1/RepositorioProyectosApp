8e260208384fc7abd1c22c4114ebd75558e9d357
SebasGh\Sebas <sebastian.garciahernandez1206@gmail.com>
2024-08-27 17:45:43 -0600

    fix: proyecto limpio

--- app/src/main/java/com/utc/repositorios/MainActivity.kt
+++ app/src/main/java/com/utc/repositorios/MainActivity.kt
@@ -20,29 +20,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             RepositorioProyectosAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RepositorioProyectosAppTheme {
-        Greeting("Android")
     }
 }
